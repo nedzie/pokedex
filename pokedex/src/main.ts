81be6958950d39118721a38c3217ad9902f6db81
Marcos Fernandes <marcosfernandes@uniplaclages.edu.br>
2022-08-20 21:43:26 -0300

    Add

--- pokedex/src/main.ts
+++ pokedex/src/main.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import _ from "lodash";
 import { frases } from "./pkFrases";
+import { dict } from "./pkTypesTraducoes";
 
 const form = document.getElementById("form") as HTMLFormElement;
 
@@ -14,27 +15,35 @@ async function obterDadosPokemon(nomePokemon: string) {
     const pokemon = resposta.data;
     
     const pId = document.createElement("p");
-    pId.innerText = pokemon.id;
+    pId.innerText = `Número: ${pokemon.id}`;
 
     const pNome = document.createElement("p");
-    pNome.innerText = _.upperFirst(pokemon.name);
+    pNome.innerText = `Nome: ${_.upperFirst(pokemon.name)}`;
 
     const pFoto = document.createElement("img");
     pFoto.src = pokemon.sprites.front_default;
+
+    const pTipo = document.createElement("p");
+    const tipo: string = pokemon.types[0].type.name;
+
+    for(let [chave, valor] of Object.entries(dict))
+      if(chave === tipo)
+        pTipo.innerText = `Tipo: ${valor}`;
 
     while(divStats.firstChild) 
       divStats.firstChild.remove();
 
     Swal.fire({
       background: "#19191a",
-      footer: frases[Math.floor(Math.random() * 8)],
+      footer: frases[Math.floor(Math.random() * frases.length)],
       confirmButtonText: _.upperFirst(pokemon.name),
       imageUrl: pokemon.sprites.front_default
     });
 
     divStats.appendChild(pId);
     divStats.appendChild(pNome);
-    divStats.appendChild(pFoto);
+    divStats.appendChild(pFoto)
+    divStats.appendChild(pTipo);
 
   } catch (error) { // Trist
     Swal.fire({
---
+++ pokedex/src/pkTypesTraducoes.ts
@@ -0,0 +1,22 @@
+export const dict = {
+  "normal": "Normal",
+  "fighting": "Lutador",
+  "flying": "Voador",
+  "poison": "Veneno",
+  "ground": "Terreno",
+  "rock": "Pedra",
+  "bug": "Inseto",
+  "ghost": "Fantasma",
+  "steel": "Aço",
+  "fire": "Fogo",
+  "water": "Água",
+  "grass": "Grama",
+  "electric": "Elétrico",
+  "psychic": "Psíquico",
+  "ice": "Gelo",
+  "dragon": "Dragão",
+  "dark": "Sombrio",
+  "fairy": "Fada",
+  "unknown": "Desconhecido",
+  "shadow": "Anti-Shiny"
+};
